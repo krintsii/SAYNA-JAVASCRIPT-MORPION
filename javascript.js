cfdcb369b71af9f9d8f32747e7fe233be39a6e7c
krintsii <fetanomenaa@gmail.com>
2022-04-27 15:12:16 +0300

    modificaton

--- javascript.js
+++ javascript.js
@@ -19,31 +19,52 @@ const valeur = [
     [0, 4, 8],
     [2, 4, 6]
 ];
+
+let playerOne, playerTwo
+const choisePlayer = document.getElementById("choisePlayer");
+const playerX = document.getElementById("playerX");
+const playerO = document.getElementById("playerO");
+const layer2 = document.getElementById('layer2');
+const endGame = document.getElementById('end-game');
+const layer3 = document.getElementById("layer3");
+const draw = document.getElementById("draw")
+
+
+function choiseX(){
+    choisePlayer.style.display='none';
+    playerOne = 'x';
+    playerTwo = 'o'
+};
+function choiseO() {
+    choisePlayer.style.display='none';
+    playerOne = 'o';
+    playerTwo = 'x'
+};
+
 // fonction qui determiner le gangant 
 function Answer () {
     for (let i = 0; i < valeur.length; i++) {
         if (ArrayX.includes(valeur[i][0]) && ArrayX.includes(valeur[i][1]) && ArrayX.includes(valeur[i][2]) ){
-            document.querySelector("main").innerHTML = `<div id="reset-box">
-                                             <h1>woaaaouuhhh mandresy x....!</h1>
-                                             <button id="reste" type="reset">reset</button>
-                                        </div>`
-                                        const reset = document.getElementById ("reste");
-                                        reset.addEventListener("click", retour);
+          
+            layer2.style.display='block';
+            document.querySelector('h4').innerHTML= `NY MPANDRESY DIA  ${playerOne} `
+            endGame.addEventListener("click",retour)
             caseScore();
-            break
+            break;
             
 
            
         } else if (ArrayO.includes(valeur[i][0]) && ArrayO.includes(valeur[i][1]) && ArrayO.includes(valeur[i][2])  ) {
-            document.querySelector("main").innerHTML = `<div id="reset-box">
-                                             <h1>woaaaouuhhh mandresy o....!</h1>
-                                             <button id="reste" type="reset">reset</button>
-                                        </div>`
-                                        const reset = document.getElementById ("reste");
-                                        reset.addEventListener("click", retour);
+           
+            layer2.style.display='block';
+            document.querySelector('h4').innerHTML= `NY MPANDRESY DIA  ${playerTwo} `
+            endGame.addEventListener("click",retour)
             caseScoreII();
             break;
 
+        } else  if (tableau[0].textContent !== '' && tableau[1].textContent !== ''&& tableau[2].textContent !== ''&& tableau[3].textContent !== ''&& tableau[4].textContent !== ''&& tableau[5].textContent !== ''&& tableau[6].textContent !== ''&& tableau[7].textContent !== ''&& tableau[8].textContent !== '') {
+            layer3.style.display='block';
+            draw.addEventListener('click', retour)
         }
         
     }
@@ -71,18 +92,22 @@ function caseScoreII() {
     scoreII++;
     const origineII = document.getElementById('cpu-score');
     origineII.textContent = scoreII
-}
+};
+
+
 
 // Vide le contenu de toute les cases
  function retour() {
-     document.getElementById("reset-box").remove()
+    layer2.style.display='none';
+    layer3.style.display='none'
     for (var i = 0; i < tableau.length; i++) {
         console.log(tableau[i]);
         tableau[i].textContent = ''
     };
     ArrayO = [];
     ArrayX = []
-}
+};
+
 function alerte(){
     alert ('ehhh ...')
 };
@@ -92,31 +117,31 @@ function addOne() {
    if (tableau[0].textContent !== ''){
        alerte();
    } else {
-    tableau[0].textContent = 'x';
+    tableau[0].textContent = playerOne;
     ArrayX.push(0);
-    if (tableau[1].textContent == "x" ){
-        tableau[2].textContent = "o";
+    if (tableau[1].textContent == playerOne ){
+        tableau[2].textContent = playerTwo;
         ArrayO.push(2);
-    } else if (tableau[3].textContent == "x" ){
-        tableau[6].textContent = "o";
+    } else if (tableau[3].textContent == playerOne ){
+        tableau[6].textContent = playerTwo;
         ArrayO.push(6);
-    } else if (tableau[4].textContent == "x" ){
-        tableau[8].textContent = "o";
+    } else if (tableau[4].textContent == playerOne ){
+        tableau[8].textContent = playerTwo;
         ArrayO.push(8);
     }else if (tableau[3].textContent == ''){
-        tableau[3].textContent = "o";
+        tableau[3].textContent = playerTwo;
         ArrayO.push(3);
     } else if (tableau[4].textContent == ""){
-        tableau[4].textContent = 'o';
+        tableau[4].textContent = playerTwo;
         ArrayO.push(4);
     } else if (tableau[8].textContent == ''){
-        tableau[8].textContent = 'o';
+        tableau[8].textContent = playerTwo;
         ArrayO.push(8);
     } else{
         for (let i = 0; i < tableau.length; i++) {
             const element = tableau[i];
             if (element.textContent == ""){
-                element.textContent = "o"
+                element.textContent = playerTwo
                 ArrayO.push(i);
                 break;
             }
@@ -134,24 +159,24 @@ function addTwo() {
     if (tableau[1].textContent !== ""){
         alerte();
     }  else {
-        tableau[1].textContent = "x"
+        tableau[1].textContent = playerOne
         ArrayX.push(1);
-        if (tableau[4].textContent == "x"){
-            tableau[7].textContent = "o";
+        if (tableau[4].textContent == playerOne){
+            tableau[7].textContent = playerTwo;
             ArrayO.push(7);
         
         }else if (tableau[2].textContent == ''){
-            tableau[2].textContent = "o";
+            tableau[2].textContent = playerTwo;
             ArrayO.push(2);
         } else if (tableau[7].textContent == ""){
-            tableau[7].textContent = 'o';
+            tableau[7].textContent = playerTwo;
             ArrayO.push(7);
         }
          else{
             for (let i = 0; i < tableau.length; i++) {
                 const element = tableau[i];
                 if (element.textContent == ""){
-                    element.textContent = "o"
+                    element.textContent = playerTwo
                     ArrayO.push(i);
                     break;
                 }
@@ -159,7 +184,8 @@ function addTwo() {
             }
         }
     };
-    Answer()
+    Answer();
+    
 
 };
 
@@ -167,27 +193,27 @@ function addThree() {
     if (tableau[2].textContent !== ''){
         alerte();
     } else {
-        tableau[2].textContent = "x";
+        tableau[2].textContent = playerOne;
         ArrayX.push(2);
-        if (tableau[1].textContent == "x"){
-            tableau[0].textContent = "o";
+        if (tableau[1].textContent == playerOne){
+            tableau[0].textContent = playerTwo;
             ArrayO.push(0);
         
-        } else if (tableau[4].textContent == 'x'){
-            tableau[6].textContent = "o";
+        } else if (tableau[4].textContent == playerOne){
+            tableau[6].textContent = playerTwo;
             ArrayO.push(6);
         
-        } else if (tableau[5].textContent == "x"){
-            tableau[8].textContent = 'o';
+        } else if (tableau[5].textContent == playerOne){
+            tableau[8].textContent = playerTwo;
             ArrayO.push(8);
         } else if (tableau[0].textContent == ''){
-            tableau[0].textContent = 'o';
+            tableau[0].textContent = playerTwo;
             ArrayO.push(0);
         } else{
             for (let i = 0; i < tableau.length; i++) {
                 const element = tableau[i];
                 if (element.textContent == ""){
-                    element.textContent = "o"
+                    element.textContent = playerTwo
                     ArrayO.push(i);
                     break;
                 }
@@ -202,22 +228,22 @@ function addFour() {
     if (tableau[3].textContent !== ""){
         alerte();
     } else {
-        tableau[3].textContent = 'x';
+        tableau[3].textContent = playerOne;
         ArrayX.push(3);
         if (tableau[4].textContent == ""){
-            tableau[4].textContent = "o";
+            tableau[4].textContent = playerTwo;
             ArrayO.push(4);
         }else if (tableau[5].textContent == ''){
-            tableau[5].textContent = "o";
+            tableau[5].textContent = playerTwo;
             ArrayO.push(5);
         } else if (tableau[6].textContent == ""){
-            tableau[6].textContent = 'o';
+            tableau[6].textContent = playerTwo;
             ArrayO.push(6);
         } else{
             for (let i = 0; i < tableau.length; i++) {
                 const element = tableau[i];
                 if (element.textContent == ""){
-                    element.textContent = "o"
+                    element.textContent = playerTwo
                     ArrayO.push(i);
                     break;
                 }
@@ -232,25 +258,25 @@ function addFive() {
     if(tableau[4].textContent !== ''){
         alerte();
     } else {
-        tableau[4].textContent = 'x';
+        tableau[4].textContent = playerOne;
         ArrayX.push(4);
         if (tableau[1].textContent == ""){
-            tableau[1].textContent = "o";
+            tableau[1].textContent = playerTwo;
             ArrayO.push(1);
         }else if (tableau[3].textContent == ''){
-            tableau[3].textContent = "o";
+            tableau[3].textContent = playerTwo;
             ArrayO.push(3);
         } else if (tableau[5].textContent == ""){
-            tableau[5].textContent = 'o';
+            tableau[5].textContent = playerTwo;
             ArrayO.push(5);
         } else if (tableau[7].textContent == ''){
-            tableau[7].textContent = 'o';
+            tableau[7].textContent = playerTwo;
             ArrayO.push(7);
         } else{
             for (let i = 0; i < tableau.length; i++) {
                 const element = tableau[i];
                 if (element.textContent == ""){
-                    element.textContent = "o"
+                    element.textContent = playerTwo
                     ArrayO.push(i);
                     break;
                 }
@@ -265,25 +291,25 @@ function addSix() {
     if (tableau[5].textContent !== ''){
         alerte();
     } else {
-        tableau[5].textContent = "x";
+        tableau[5].textContent = playerOne;
         ArrayX.push(5);
         if (tableau[2].textContent == ""){
-            tableau[2].textContent = "o";
+            tableau[2].textContent = playerTwo;
             ArrayO.push(2);
         }else if (tableau[3].textContent == ''){
-            tableau[3].textContent = "o";
+            tableau[3].textContent = playerTwo;
             ArrayO.push(3);
         } else if (tableau[4].textContent == ""){
-            tableau[4].textContent = 'o';
+            tableau[4].textContent = playerTwo;
             ArrayO.push(4);
         } else if (tableau[8].textContent == ''){
-            tableau[8].textContent = 'o';
+            tableau[8].textContent = playerTwo;
             ArrayO.push(8);
         } else{
             for (let i = 0; i < tableau.length; i++) {
                 const element = tableau[i];
                 if (element.textContent == ""){
-                    element.textContent = "o"
+                    element.textContent = playerTwo
                     ArrayO.push(i);
                     break;
                 }
@@ -298,25 +324,25 @@ function addSeven() {
     if (tableau[6].textContent !== ''){
         alerte();
     } else {
-        tableau[6].textContent = "x";
+        tableau[6].textContent = playerOne;
         ArrayX.push(6);
         if (tableau[0].textContent == ""){
-            tableau[0].textContent = "o";
+            tableau[0].textContent = playerTwo;
             ArrayO.push(0);
         }else if (tableau[2].textContent == ''){
-            tableau[2].textContent = "o";
+            tableau[2].textContent = playerTwo;
             ArrayO.push(2);
         } else if (tableau[3].textContent == ""){
-            tableau[3].textContent = 'o';
+            tableau[3].textContent = playerTwo;
             ArrayO.push(3);
         } else if (tableau[4].textContent == ''){
-            tableau[4].textContent = 'o';
+            tableau[4].textContent = playerTwo;
             ArrayO.push(4);
         } else{
             for (let i = 0; i < tableau.length; i++) {
                 const element = tableau[i];
                 if (element.textContent == ""){
-                    element.textContent = "o"
+                    element.textContent = playerTwo
                     ArrayO.push(i);
                     break;
                 }
@@ -331,25 +357,25 @@ function addHeight() {
     if (tableau[7].textContent !== ""){
         alerte();
     }else {
-        tableau[7].textContent = "x";
+        tableau[7].textContent = playerOne;
         ArrayX.push(7);
         if (tableau[1].textContent == ""){
-            tableau[1].textContent = "o";
+            tableau[1].textContent = playerTwo;
             ArrayO.push(1);
         }else if (tableau[3].textContent == ''){
-            tableau[3].textContent = "o";
+            tableau[3].textContent = playerTwo;
             ArrayO.push(3);
         } else if (tableau[6].textContent == ""){
-            tableau[6].textContent = 'o';
+            tableau[6].textContent = playerTwo;
             ArrayO.push(6);
         } else if (tableau[8].textContent == ''){
-            tableau[8].textContent = 'o';
+            tableau[8].textContent = playerTwo;
             ArrayO.push(8);
         } else{
             for (let i = 0; i < tableau.length; i++) {
                 const element = tableau[i];
                 if (element.textContent == ""){
-                    element.textContent = "o"
+                    element.textContent = playerTwo
                     ArrayO.push(i);
                     break;
                 }
@@ -364,25 +390,25 @@ function addNine() {
     if (tableau[8].textContent !== ''){
         alerte();
     } else {
-        tableau[8].textContent = "x";
+        tableau[8].textContent = playerOne;
         ArrayX.push(8);
         if (tableau[0].textContent == ""){
-            tableau[0].textContent = "o";
+            tableau[0].textContent = playerTwo;
             ArrayO.push(0);
         }else if (tableau[2].textContent == ''){
-            tableau[2].textContent = "o";
+            tableau[2].textContent = playerTwo;
             ArrayO.push(2);
         } else if (tableau[5].textContent == ""){
-            tableau[5].textContent = 'o';
+            tableau[5].textContent = playerTwo;
             ArrayO.push(5);
         } else if (tableau[4].textContent == ''){
-            tableau[4].textContent = 'o';
+            tableau[4].textContent = playerTwo;
             ArrayO.push(4);
         } else{
             for (let i = 0; i < tableau.length; i++) {
                 const element = tableau[i];
                 if (element.textContent == ""){
-                    element.textContent = "o"
+                    element.textContent = playerTwo
                     ArrayO.push(i);
                     break;
                 }
@@ -402,4 +428,5 @@ tableau[5].addEventListener("click",addSix)
 tableau[6].addEventListener("click",addSeven)
 tableau[7].addEventListener("click",addHeight)
 tableau[8].addEventListener("click",addNine)
-console.log(ArrayO);
+playerX.addEventListener('click',choiseX);
+playerO.addEventListener('click',choiseO)
